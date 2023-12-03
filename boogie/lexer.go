b06ad15dfeab1df33364b18cb40e62f208c6f486
VahidR <vahid.rafiei@gmail.com>
2023-12-03 18:10:39 +0100

    Enhance boogie lexer with processing capabilities
    Enhanced the boogie lexer by introducing state machine logic, keyword recognition, context tracking and other processing attributes. The changes now route the input through a lexer process before printing, rather than direct print; enhancing user interaction potential and laying groundwork for future language processing enhancements.

--- boogie/lexer.go
+++ boogie/lexer.go
@@ -1,21 +1,66 @@
 package boogie
 
-import "fmt"
+import (
+	"strings"
+)
+
+// A State-Machine Enum
+type ContextState int
+
+const (
+	START ContextState = iota
+	KEYWORD
+)
 
 type Lexer struct {
-	In chan string
+	In             chan string // the channel that we use to read from STDIN
+	currentChar    string
+	currentContext ContextState // the current state of the lexer
+	keywords       []string
+	buffer         string // the buffer that we use to store the current token
 }
 
 // NewLexer Constructor for the Lexer
 func NewLexer(in chan string) *Lexer {
 	return &Lexer{
-		In: in,
+		In:             in,
+		currentContext: START,
+		keywords:       []string{"QUIT"}, // the keywords that we want to recognize
 	}
 }
 
+// Run runs the lexer
 func (lexer *Lexer) Run() {
 	for {
-		message := <-lexer.In
-		fmt.Println(message)
+		lineOfCode := <-lexer.In
+		err := lexer.process(lineOfCode)
+		if err != nil {
+			panic(err)
+		}
 	}
+}
+
+func (lexer *Lexer) process(lineOfCode string) error {
+	for _, r := range lineOfCode {
+		lexer.currentChar = string(r) // convert it to string
+		switch lexer.currentContext {
+		case START:
+			lexer.buffer += lexer.currentChar
+			if lexer.isStringInSlice(lexer.buffer, lexer.keywords) {
+				lexer.currentContext = KEYWORD
+			}
+		case KEYWORD:
+			// do something with the keyword
+		}
+	}
+	return nil
+}
+
+func (lexer *Lexer) isStringInSlice(str string, list []string) bool {
+	for _, value := range list {
+		if value == strings.ToUpper(str) {
+			return true
+		}
+	}
+	return false
 }
