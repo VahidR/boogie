b97a2f2199c8b9287954d98e1bb1389cfe401eea
VahidR <vahid.rafiei@gmail.com>
2023-12-16 20:23:38 +0100

    Adding the process method to parser.

--- boogie/parser.go
+++ boogie/parser.go
@@ -3,18 +3,28 @@ package boogie
 import "github.com/davecgh/go-spew/spew"
 
 type Parser struct {
-	In chan Lexeme
+	In  chan Lexeme
+	ast map[string][]string
 }
 
 func NewParser() *Parser {
+	ast := make(map[string][]string)
+	ast["start"] = make([]string, 0)
 	return &Parser{
-		In: make(chan Lexeme),
+		In:  make(chan Lexeme),
+		ast: ast,
 	}
 }
 
 func (parser *Parser) Run() {
 	for {
 		lex := <-parser.In
-		spew.Dump(lex)
+		_ = parser.process(lex)
+		spew.Dump(parser.ast)
 	}
+}
+
+func (parser *Parser) process(lex Lexeme) error {
+	parser.ast["start"] = append(parser.ast["start"], lex.Val)
+	return nil
 }
